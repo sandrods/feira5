bac6201fdac3350e62d9ad45ea93c4629d31cd67
Sandro Duarte <sandrods@gmail.com>
2018-07-04 19:51:28 -0300

    contas

--- config/routes.rb
+++ config/routes.rb
@@ -71,5 +71,7 @@ Rails.application.routes.draw do
   end
   resources :transferencias, controller: 'registros/transferencias'
 
+  resources :contas
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
