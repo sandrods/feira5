040d5946a4aebb65527a4903fb192c5afc10ad02
Sandro Duarte <sandrods@gmail.com>
2018-07-09 21:54:27 -0300

    colorir progress_bar completado

--- app/helpers/colecoes_helper.rb
+++ app/helpers/colecoes_helper.rb
@@ -1,9 +1,10 @@
 module ColecoesHelper
 
   def progress(value)
+    color = (value >= 100) ? 'bg-success' : ''
     <<-HTML
     <div class="progress">
-      <div class="progress-bar progress-bar-striped" role="progressbar" style="width: #{value}%">
+      <div class="progress-bar #{color}" role="progressbar" style="width: #{value}%">
       </div>
     </div>
     HTML
