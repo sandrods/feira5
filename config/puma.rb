ffeefbfda1478ec4aa08e86a6af4e859f22c8779
Sandro Duarte <sandrods@gmail.com>
2019-01-20 23:20:47 -0200

    single thread

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+# workers Integer(ENV['WEB_CONCURRENCY'] || 1)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+threads 1, threads_count
 
 preload_app!
 
@@ -8,11 +8,11 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
